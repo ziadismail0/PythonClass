ede699e42c98cfb4f6cb33658d7597ff87ac5fd1
ziadismail0 <113112438+ziadismail0@users.noreply.github.com>
2022-10-11 19:06:49 +0300

    Create Lab3.py

---
+++ Lab3.py
@@ -0,0 +1,48 @@
+#Задание 1
+
+a = [1,0,3,4,0,6,7,8,9,0]
+def adder(A):
+	p = sum(A)
+	print(p)
+	
+	
+adder(a)
+
+#задание 2
+
+def counit(B):
+	x= B.count(0)
+	print(x)
+	
+	
+countit(a)
+
+
+#задание 3
+
+def triangular(n):
+	for i in range(n):
+		for j in range(i+1):
+			print(j+1, end=' ')
+		print()
+			
+triangular(4)
+			
+#задание 4
+
+def pyramid(q):
+	
+	for i in range(1, q + 1):
+		for j in range(1,q + 1-i):
+			print(' ', end = ' ')
+			
+		for j in range (1,i + 1):
+			print(j, end=' ')
+			
+		for j in range(i -1, 0, -1):
+				print(j, end= ' ')
+		print()
+				
+pyramid(5)
+				
+				
