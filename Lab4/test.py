092c5eca55964f014bd1f6e345fade0ac6908459
ziadismail0 <113112438+ziadismail0@users.noreply.github.com>
2022-12-21 15:52:10 +0300

    Create test.py

---
+++ Lab4/test.py
@@ -0,0 +1,19 @@
+from figure import *
+
+circle_area()
+circle_area(12)
+
+circle_perimeter()
+circle_perimeter(11)
+
+triangle_perimeter()
+triangle_perimeter(2,2,7)
+
+triangle_area()
+triangle_area(5,9,6)
+
+square_perimeter()
+square_perimeter(4)
+
+square_area()
+square_area(4)
